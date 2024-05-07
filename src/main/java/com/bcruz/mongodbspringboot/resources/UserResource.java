0ba665f41b111c076f975d5ede6d39d564c54974
Bruno Cruz <bjefersoncruz@gmail.com>
2024-05-07 19:03:14 -0300

    getPosts()

--- src/main/java/com/bcruz/mongodbspringboot/resources/UserResource.java
+++ src/main/java/com/bcruz/mongodbspringboot/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.bcruz.mongodbspringboot.domain.Post;
 import com.bcruz.mongodbspringboot.domain.User;
 import com.bcruz.mongodbspringboot.dto.UserDTO;
 import com.bcruz.mongodbspringboot.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj=service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity <List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
