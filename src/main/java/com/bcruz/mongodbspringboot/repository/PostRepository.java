dae6ea7ab310a41a8047bc346846124704e5a930
Bruno Cruz <bjefersoncruz@gmail.com>
2024-05-07 20:32:40 -0300

    full Search

--- src/main/java/com/bcruz/mongodbspringboot/repository/PostRepository.java
+++ src/main/java/com/bcruz/mongodbspringboot/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.bcruz.mongodbspringboot.repository;
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,6 +13,11 @@ import com.bcruz.mongodbspringboot.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
 	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
-	List<Post> findByTitle(String text);
+	List<Post> findByTitle(String title);
+	
+	@Query("{ $and: [{date: {$gte : ?1} },{date: {$lte : ?2} },{$or: [{ 'title': {$regex: ?0, $options: 'i'}  },{ 'body': {$regex: ?0, $options: 'i'}  },{ 'comments.text': {$regex: ?0, $options: 'i'}  } ] } ] }")
+	List<Post> fullSearch(String title,Date minDate,Date maxDate);
+	
 }
